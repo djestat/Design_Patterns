779a9b486f9ac06523f8cc8908abb37db47e5a93
Igor <vionce@bk.ru>
2019-11-17 23:16:24 +0300

    update

--- Lesson6/Decorator.playground/Contents.swift
+++ Lesson6/Decorator.playground/Contents.swift
@@ -6,18 +6,71 @@ import Foundation
  Создайте протокол Coffee и класс SimpleCoffee, реализующий протокол Coffee. Создайте протокол CoffeeDecorator, который «наследуется» от протокола Coffee. Далее создайте реализации декоратора — Milk, Whip, Sugar и другие, которые будут ингредиентами, которые добавляют в кофе. В протоколе Coffee объявите переменную cost: Int — это цена кофе. Каждый ингредиент должен увеличивать цену кофе на свою стоимость. Реализуйте описанную систему объектов с помощью паттерна Decorator аналогично тому, как это было сделано на уроке.
  */
 
-protocol Coffee: Protocol {
-    func getCoffee()
+protocol Coffee {
+    var cost: Int { get }
 }
 
-class SimpleCoffee {
-    let cost: Int = 100
+class SimpleCoffee: Coffee {
+    var cost: Int {
+        return 100
+    }
 }
 
 protocol CoffeeDecorator: Coffee {
-    
+    var base: Coffee { get }
+    init (base: Coffee)
 }
 
 
+class Milk: CoffeeDecorator {
+    var base: Coffee
+    
+    required init(base: Coffee) {
+        self.base = base
+    }
+    
+    var cost: Int {
+        return base.cost + 20
+    }
+}
 
-//
+class Sugar: CoffeeDecorator {
+    var base: Coffee
+    
+    required init(base: Coffee) {
+        self.base = base
+    }
+    
+    var cost: Int {
+        return base.cost + 10
+    }
+}
+
+class Whip: CoffeeDecorator {
+    var base: Coffee
+    
+    required init(base: Coffee) {
+        self.base = base
+    }
+    
+    var cost: Int {
+        return base.cost + 30
+    }
+}
+
+let blackCoffee = SimpleCoffee()
+let coffeeWithShugar = Sugar(base: blackCoffee)
+let coffeeWithMilk = Milk(base: blackCoffee)
+let coffeeWithWhip = Whip(base: blackCoffee)
+let coffeeWithSugarAndMilk = Milk(base: coffeeWithShugar)
+let coffeeWithSugarAndWhip = Whip(base: coffeeWithShugar)
+let coffeeWithAllIngridients = Whip(base: coffeeWithSugarAndMilk)
+
+print("\(blackCoffee.cost) руб.")
+print("\(coffeeWithShugar.cost) руб.")
+print("\(coffeeWithMilk.cost) руб.")
+print("\(coffeeWithWhip.cost) руб.")
+print("\(coffeeWithSugarAndMilk.cost) руб.")
+print("\(coffeeWithSugarAndWhip.cost) руб.")
+print("\(coffeeWithAllIngridients.cost) руб.")
+
