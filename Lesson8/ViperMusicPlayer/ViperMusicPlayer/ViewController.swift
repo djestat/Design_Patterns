a5840d6725c72a0f908873395fedf7c9e8bc5779
Igor <vionce@bk.ru>
2019-11-24 02:41:12 +0300

    init

---
+++ Lesson8/ViperMusicPlayer/ViperMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperMusicPlayer
+//
+//  Created by Igor on 24.11.2019.
+//  Copyright © 2019 Igor Gapanovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
